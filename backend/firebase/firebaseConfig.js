9a7fd89e349d7cd8efb22b51cd52f46407c9af35
kiran <kirangawande9307@gmail.com>
2025-08-06 23:30:22 +0530

    add fsa.json file in .env

--- backend/firebase/firebaseConfig.js
+++ backend/firebase/firebaseConfig.js
@@ -1,6 +1,19 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
-const serviceAccount =require("./firebase-service-account.json")
+
+const serviceAccountJson = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+  "base64"
+).toString("utf8");
+
+// Parse JSON string from .env
+const serviceAccount = JSON.parse(serviceAccountJson);
+
+// Fix the \n issue in private_key
+
+
+console.log("Parsed Service Account:", serviceAccount);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
